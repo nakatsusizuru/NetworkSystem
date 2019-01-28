5e5c50b5670a2667bcf3aa614f382ce08711e567
master@crack.ee <master@crack.ee>
2019-01-28 15:18:36 +0800

    add Customer

--- VerifyEngine/Service/Cry.Signal.Service.cpp
+++ VerifyEngine/Service/Cry.Signal.Service.cpp
@@ -1,7 +1,16 @@
 #include <Global>
 #include <Service/Cry.Signal.Service.h>
+#include <evpp/tcp_conn.h>
 namespace Cry
 {
+	Work::Work(const NetworkServiceEngine * Service, const evpp::TCPConnPtr & Conn)
+	{
+
+	}
+	void Work::Receive(const evpp::Buffer * pData)
+	{
+
+	}
 	NetworkServiceEngine::NetworkServiceEngine(const std::string & lpszAddress, const std::string & lpszFlags, const u32 & uSize) : m_Loop(std::make_unique<evpp::EventLoopThread>()), m_Server(std::make_unique<evpp::TCPServer>(m_Loop->loop(), lpszAddress, lpszFlags, uSize))
 	{
 		m_Server->SetConnectionCallback(std::bind(&NetworkServiceEngine::OnConnection, this, std::placeholders::_1));
@@ -35,10 +44,61 @@ namespace Cry
 	}
 	void NetworkServiceEngine::OnMessage(const evpp::TCPConnPtr & Conn, const evpp::Buffer * Buffer)
 	{
-
+		if (std::shared_ptr<Work> Work = this->GetWork(Conn->id()); Work != nullptr)
+			Work->Receive(Buffer);
+		else
+			Conn->Close();
 	}
 	void NetworkServiceEngine::OnConnection(const evpp::TCPConnPtr & Conn)
 	{
-
+		if (Conn->IsConnected())
+		{
+			if (!this->AddWork(Conn->id(), std::make_shared<Work>(this, Conn)))
+			{
+				Conn->Close();
+			}
+		}
+		if (Conn->IsDisconnected())
+		{
+			if (!this->DelWork(Conn->id()))
+			{
+				Conn->Close();
+			}
+		}
+	}
+	bool NetworkServiceEngine::AddWork(u64 Index, std::shared_ptr<Work> Work)
+	{
+		std::lock_guard<std::mutex> Guard(m_Mutex);
+		{
+			if (m_Work.find(Index) == m_Work.cend())
+			{
+				m_Work.emplace(Index, Work);
+				return true;
+			}
+		}
+		return false;
+	}
+	bool NetworkServiceEngine::DelWork(u64 Index)
+	{
+		std::lock_guard<std::mutex> Guard(m_Mutex);
+		{
+			if (auto iter = m_Work.find(Index); iter != m_Work.cend())
+			{
+				m_Work.erase(iter);
+				return true;
+			}
+		}
+		return false;
+	}
+	std::shared_ptr<Work> NetworkServiceEngine::GetWork(u64 Index)
+	{
+		std::lock_guard<std::mutex> Guard(m_Mutex);
+		{
+			if (auto iter = m_Work.find(Index); iter != m_Work.cend())
+			{
+				return iter->second;
+			}
+		}
+		return nullptr;
 	}
 }
--- VerifyEngine/Service/Cry.Signal.Service.h
+++ VerifyEngine/Service/Cry.Signal.Service.h
@@ -4,6 +4,41 @@
 #include <memory>
 namespace Cry
 {
+	class NetworkServiceEngine;
+
+	struct CustomerData
+	{
+	public:
+		CustomerData() = default;
+		CustomerData(const std::string & lpszUsername, const std::string & lpszPassword) : Username(lpszUsername), Password(lpszPassword) {};
+		bool operator == (const CustomerData & Other) const
+		{
+			return Other.Username == Username && Other.Password == Password;
+		}
+		bool operator != (const CustomerData & Other) const
+		{
+			return !(*this == Other);
+		}
+	private:
+		std::string			Username;
+		std::string			Password;
+		u32					Expires;
+	};
+
+	class Work
+	{
+	public:
+		explicit Work(const NetworkServiceEngine * Service, const evpp::TCPConnPtr & Conn);
+		~Work() = default;
+	public:
+		void Receive(const evpp::Buffer * pData);
+	private:
+		/// 缓冲区
+		std::string													m_lpszBody;
+		/// 客户信息
+		Cry::CustomerData											m_Customer;
+	};
+
 	class NetworkServiceEngine
 	{
 	public:
@@ -16,7 +51,14 @@ namespace Cry
 		void OnMessage(const evpp::TCPConnPtr & Conn, const evpp::Buffer * Buffer);
 		void OnConnection(const evpp::TCPConnPtr & Conn);
 	private:
-		std::unique_ptr<evpp::EventLoopThread>						m_Loop;
-		std::unique_ptr<evpp::TCPServer>							m_Server;
+		bool AddWork(u64 Index, std::shared_ptr<Work> Work);
+		bool DelWork(u64 Index);
+		std::shared_ptr<Work> GetWork(u64 Index);
+	private:
+		std::unique_ptr<evpp::EventLoopThread>								m_Loop;
+		std::unique_ptr<evpp::TCPServer>									m_Server;
+	private:
+		std::mutex															m_Mutex;
+		std::unordered_map<u64, std::shared_ptr<Work>>						m_Work;
 	};
 }
